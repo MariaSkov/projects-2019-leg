726c5f6f96e667d533965fbd5fdf6b5a2b4bebca
cluo192 <zbl192@alumni.ku.dk>
2019-03-23 18:25:56 +0100

    read csv in vs code

---
+++ d.py
@@ -0,0 +1,31 @@
+#%%
+a = 'dear'
+print('dear')
+
+
+
+
+
+
+
+#%%
+
+b = 5*9
+print(b)
+
+
+#%%
+import pandas as pd
+
+#%%
+import os
+
+#%%
+import csv
+
+
+#%%
+df=pd.read_csv('C:/Users/aegea/.jupyter/data/food_supply_kilocalories_per_person_and_day.csv')
+df.head(10)
+
+#%%
