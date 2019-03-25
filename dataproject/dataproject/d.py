3926b386ede7898e430f34eb855c039bb5589bf3
cluo192 <zbl192@alumni.ku.dk>
2019-03-25 14:06:18 +0100

    plan

--- dataproject/dataproject/d.py
+++ dataproject/dataproject/d.py
@@ -1,31 +1,34 @@
 #%%
 a = 'dear'
 print('dear')
-
-
-
-
-
-
-
 #%%
 
 b = 5*9
 print(b)
-
 
 #%%
 import pandas as pd
 
 #%%
 import os
-
 #%%
 import csv
-
-
 #%%
 df=pd.read_csv('C:/Users/aegea/.jupyter/data/food_supply_kilocalories_per_person_and_day.csv')
 df.head(10)
+df.info()
+df.loc[:, ['2002']]
+df['growth']=df['2009']/df['2010']
+mydict={}
+for i in range(1961, 2013):
+    mydict[str(i)] = f'e{i}'
+mydict
+df.describe()
+import numpy as np
+import matplotlib.pyplot as plt
+df1=pd.read_csv('E:/Block 8 IPNA/Data/total_health_spending_percent_of_gdp.csv')
+new=pd.merge(df, df1, on=['country'], how='inner')
+new
+
 
 #%%
--- dataproject/dataproject/example.py
+++ dataproject/dataproject/example.py
@@ -1,2 +1,19 @@
 def hello_world():
     return "hello world"
+    1. read the data
+2. rename the variables as the VN are the same
+1
+2
+3
+4
+	myDict = {}
+for i in range(2008, 2017): # range goes from 2008 to but not including 2017
+    myDict[str(i)] = f'e{i}' 
+myDict
+3. merge the data
+4. drop if nan
+5. summary statistics: df.describe() (groupby)
+6. generate a variable called real_spending
+7. plot the real_spending for certain country
+8. group by GDP/capita;
+9. aggregate for each group and plot the aggregated results
