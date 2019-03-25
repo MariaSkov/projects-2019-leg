c29c15c8e7b461d5f23811f0319ee0a8720fa773
Eugenindaeyo <bmf526@alumni.ku.dk>
2019-03-25 13:31:53 +0100

    Practice file has been uploaded: we will work on this file when we share a decent result of each of us

--- dataproject/dataproject/d.py
+++ dataproject/dataproject/d.py
@@ -29,3 +29,7 @@ df=pd.read_csv('C:/Users/aegea/.jupyter/data/food_supply_kilocalories_per_person
 df.head(10)
 
 #%%
+
+## maybe we can work with the codes from "Fun application" lecture note: CO2 emmisions and GDP part
+## I will write sth down on the jubyternote file(ipynb) in dataproject folder, right below the dataproject.ipynb file: this is the folder where we should write down the final code as a result. I will name my practice ipynb file as pr:dataprojet.ipynb. Plz add up or change stuffs freely there
+
